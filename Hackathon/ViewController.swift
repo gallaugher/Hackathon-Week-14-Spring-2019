08e3bf07e29e6ce0456e65f19d8972a3caa698f1
John Gallaugher <john.gallaugher@bc.edu>
2019-04-22 16:31:29 -0400

    Initial Commit

---
+++ Hackathon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hackathon
+//
+//  Created by John Gallaugher on 4/22/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
